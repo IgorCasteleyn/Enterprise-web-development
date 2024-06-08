77f7cb819a890ea01de9d8e48440e1f24c30ff71
IIC004 <igorcasteleyn@gmail.com>
2024-06-08 14:45:00 +0200

    first commit

---
+++ src/main/java/com/zomerFestival/ZomerFestivalApp.java
@@ -0,0 +1,11 @@
+package com.zomerFestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomerFestivalApp {
+  public static void main(String[] args) {
+    SpringApplication.run(ZomerFestivalApp.class, args);
+  }
+}
